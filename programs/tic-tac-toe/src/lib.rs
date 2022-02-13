62abb8844a2a2cc9c65268dd40f813ac2a48ddfc
Alber Erre <arblugo@gmail.com>
2022-02-13 20:49:56 +0000

    test: add test on game creation & structs must be public

--- programs/tic-tac-toe/src/lib.rs
+++ programs/tic-tac-toe/src/lib.rs
@@ -10,6 +10,9 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod tic_tac_toe {
   use super::*;
   pub fn setup_game(ctx: Context<SetupGame>, player_two: Pubkey) -> ProgramResult {
+    let game = &mut ctx.accounts.game;
+    game.players = [ctx.accounts.player_one.key(), player_two];
+    game.turn = 1;
     Ok(())
   }
 }
--- programs/tic-tac-toe/src/state.rs
+++ programs/tic-tac-toe/src/state.rs
@@ -4,10 +4,10 @@ use num_derive::*;
 #[account]
 #[derive(Default)]
 pub struct Game {
-  players: [Pubkey; 2],          // 64
-  turn: u8,                      // 1
-  board: [[Option<Sign>; 3]; 3], // 9 * (1 + 1) = 18
-  state: GameState,              // 32 + 1
+  pub players: [Pubkey; 2],          // 64
+  pub turn: u8,                      // 1
+  pub board: [[Option<Sign>; 3]; 3], // 9 * (1 + 1) = 18
+  pub state: GameState,              // 32 + 1
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
